d65fc01a01abaf5ecec4ebbc55148336a6f9a2ef
PakhinZE <oumwasd1@gmail.com>
2021-11-17 00:56:57 +0700

    2.1

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/decision_tree_smote.py
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/random_forest.py
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/random_forest_smote.py
