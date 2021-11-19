532741d551029f9b2a3569c42c8d475329ed82ed
PakhinZE <oumwasd1@gmail.com>
2021-11-19 22:27:02 +0700

    2.10

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/quad_da.py
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/quad_da_smote.py
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/random_forest_smote.py
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/svm.py
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/svm_smote.py
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/adaboot.py
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/adaboot_smote.py
