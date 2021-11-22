81c261b543447efc01094a3f125d804aa27db6ce
PakhinZE <oumwasd1@gmail.com>
2021-11-23 01:27:48 +0700

    2.12

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/gradientboosting.py
-/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/gradientboosting_smote.py
+/home/oumwasd/.cache/pypoetry/virtualenvs/cs701-project-FCN4t2Nr-py3.9/bin/python3 /home/oumwasd/Code/Python/Project/algro/extremely_randomized_trees.py
