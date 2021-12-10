0b96fd53121fc1d588fa3bbeddcafad28589abed
PakhinZE <oumwasd1@gmail.com>
2021-12-10 22:27:24 +0700

    4.3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CS701 Project of Group 7
 Python 3.9+\
 R 4.1.2+\
-Python ไลบารี่ที่ต้องใช้\
+Python ไลบรารี่ที่ต้องใช้\
 scikit-learn\
 hmeasure\
 pandas\
@@ -9,10 +9,11 @@ pylint\
 imbalanced-learn\
 ipykernel\
 xgboost\
-R ไลบารี่ที่ต้องใช้\
+R ไลบรารี่ที่ต้องใช้\
 tidyverse\
 devtools\
 scmamp\
 สามารถใช้ poetry เพื่อติดตั้งไลบารี่สำหรับ Python ได้ (ไฟล์ pyproject.toml จำเป็นสำหรับการใช้งาน poetry)\
 สามารถ run แต่ละไฟล์ที่มีชื่ออัลกอริทึมในโฟลเดอร์ algro (ตัวอย่างเช่น elasticnet_logistic_regression_smote.py)\
-เมื่อ run เสร็จแล้วจะผลลัพธ์จะอยู่ในโฟลเดอร์ result
+เมื่อ run เสร็จแล้วจะผลลัพธ์จะอยู่ในโฟลเดอร์ result\
+ถ้าต้องการนำไฟล์ผลลัพธ์มาทดสอบทางสถิติจะต้องใช้ภาษา R โดยต้องติดตั้งไลบรารี่ scmamp ด้วยคำสั่ง devtools::install_github("b0rxa/scmamp")\
--- static_compar.r
+++ static_compar.r
@@ -6,12 +6,14 @@ result_paths <- fs::dir_ls(path = fs::path(parent_path, "result"),
     regexp = "Scores")
 # read socres file
 result_all <- purrr::map(result_paths, readr::read_csv)
-result_f1 <- as_tibble(purrr::map(result_all, purrr::chuck("F1")))
-result_auc <- as_tibble(purrr::map(result_all, purrr::chuck("AUC")))
-result_h <- as_tibble(purrr::map(result_all, purrr::chuck("H-measure")))
-result_ks <- as_tibble(purrr::map(result_all, purrr::chuck("KS_score")))
-result_brier <- as_tibble(purrr::map(result_all, purrr::chuck("Brier_score")))
-result_log <- as_tibble(purrr::map(result_all, purrr::chuck("Log_loss")))
+result_f1 <- tibble::as_tibble(purrr::map(result_all, purrr::chuck("F1")))
+result_auc <- tibble::as_tibble(purrr::map(result_all, purrr::chuck("AUC")))
+result_h <- tibble::as_tibble(purrr::map(result_all, purrr::chuck("H-measure")))
+result_ks <- tibble::as_tibble(purrr::map(result_all, purrr::chuck("KS_score")))
+result_brier <- tibble::as_tibble(purrr::map(result_all,
+    purrr::chuck("Brier_score")))
+result_log <- tibble::as_tibble(purrr::map(result_all,
+    purrr::chuck("Log_loss")))
 # rename columns
 regexr_body <- rlang::expr(str_match(text,
     pattern = "(?!.*/)(.*)(?:\\sScores)")[, 2])
@@ -23,8 +25,47 @@ result_ks <- dplyr::rename_with(result_ks, .fn = regexr)
 result_brier <- dplyr::rename_with(result_brier, .fn = regexr)
 result_log <- dplyr::rename_with(result_log, .fn = regexr)
 # statistical testing
-result_1 <- scmamp::imanDavenportTest(result_f1)
-result_2 <- scmamp::postHocTest(result_f1, 
+## F1
+omnibus_f1 <- scmamp::imanDavenportTest(result_f1)
+pairwise_f1 <- scmamp::postHocTest(result_f1,
     test = "aligned ranks", correct = "rom")
-scmamp::plotRanking(pvalues = result_2$corrected.pval, 
-    summary = result_2$summary, alpha = 0.05)
+pairwise_result_f1 <- as_tibble(pairwise_f1$corrected.pval, rownames = NA)
+## AUC
+omnibus_auc <- scmamp::imanDavenportTest(result_auc)
+pairwise_auc <- scmamp::postHocTest(result_auc,
+    test = "aligned ranks", correct = "rom")
+pairwise_result_auc <- as_tibble(pairwise_auc$corrected.pval, rownames = NA)
+## H-measure
+omnibus_h <- scmamp::imanDavenportTest(result_h)
+pairwise_h <- scmamp::postHocTest(result_h,
+    test = "aligned ranks", correct = "rom")
+pairwise_result_h <- as_tibble(pairwise_h$corrected.pval, rownames = NA)
+## KS_score
+omnibus_ks <- scmamp::imanDavenportTest(result_ks)
+pairwise_ks <- scmamp::postHocTest(result_ks,
+    test = "aligned ranks", correct = "rom")
+pairwise_result_ks <- as_tibble(pairwise_ks$corrected.pval, rownames = NA)
+## Brier_score
+omnibus_brier <- scmamp::imanDavenportTest(result_brier)
+pairwise_brier <- scmamp::postHocTest(result_brier,
+    test = "aligned ranks", correct = "rom")
+pairwise_result_brier <- as_tibble(pairwise_brier$corrected.pval, rownames = NA)
+## Log_loss
+omnibus_log <- scmamp::imanDavenportTest(result_log)
+pairwise_log <- scmamp::postHocTest(result_log,
+    test = "aligned ranks", correct = "rom")
+pairwise_result_log <- as_tibble(pairwise_log$corrected.pval, rownames = NA)
+# Ploting
+## cp /tmp/Rtmp0mjEzB/vscode-R/plot.png /workspaces/Project/
+plot_f1 <- scmamp::plotRanking(pvalues = pairwise_f1$corrected.pval,
+    summary = pairwise_f1$summary, alpha = 0.05)
+plot_auc <- scmamp::plotRanking(pvalues = pairwise_auc$corrected.pval,
+    summary = pairwise_auc$summary, alpha = 0.05)
+plot_h <- scmamp::plotRanking(pvalues = pairwise_h$corrected.pval,
+    summary = pairwise_h$summary, alpha = 0.05)
+plot_ks <- scmamp::plotRanking(pvalues = pairwise_ks$corrected.pval,
+    summary = pairwise_ks$summary, alpha = 0.05)
+plot_brier <- scmamp::plotRanking(pvalues = pairwise_brier$corrected.pval,
+    summary = pairwise_brier$summary, alpha = 0.05)
+plot_log <- scmamp::plotRanking(pvalues = pairwise_log$corrected.pval,
+    summary = pairwise_log$summary, alpha = 0.05)
